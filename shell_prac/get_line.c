2b225299bd2499c6440fa92f2d044d73e7d89fae
matxa <matxa21@gmail.com>
2019-11-14 17:37:47 +0000

    delete test file

---
+++ shell_prac/get_line.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+* get_line - get user inputed line
+* @buffer - where the input is being stored
+* @n: size of the input
+* @stream: Look into file I/O -- Question
+* Return: the number of chars read
+*/
+
+int get_line(char **buffer, size_t *n, FILE *stream)
+{
+	
+}
